29118c67b7667d78c9d169ad4ad08ec2fed1c768
Devan Rivera <devanrivera98@gmail.com>
2024-06-04 21:45:48 +0000

    installed tailwind and added color themes

---
+++ client/src/components/Services.tsx
@@ -0,0 +1,9 @@
+export default function Services() {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl">Services</h1>
+      </div>
+    </>
+  );
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      BrandRed: '#BF0101',
+      BrandBlue: '#57D0FC',
+      Copy: '#0F2B35',
+      LogoGray: '#4E5059',
+      PhBlue: '#0978A0',
+    },
+  },
+  plugins: [],
+};
