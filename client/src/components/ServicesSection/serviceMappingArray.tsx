cbc7cb054663016095ba6cfdd51d4dacb67bfa42
Devan Rivera <devanrivera98@gmail.com>
2024-06-11 22:28:46 +0000

    created mapping array and imported images

---
+++ client/src/components/ServicesSection/serviceMappingArray.tsx
@@ -0,0 +1,45 @@
+interface ServiceItem {
+  id: number;
+  title: string;
+  text: string;
+  image: string;
+}
+
+export const serviceMap: ServiceItem[] = [
+  {
+    id: 1,
+    title: 'Customized HVAC Services Contracting',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a1.png',
+  },
+  {
+    id: 2,
+    title: 'HVAC Installation & Repair',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a2.png',
+  },
+  {
+    id: 3,
+    title: 'Daily Boiler Engineering Inspections',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a3.png',
+  },
+  {
+    id: 4,
+    title: 'Building Inspecition & Evaluations',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a4.png',
+  },
+  {
+    id: 5,
+    title: 'Preventative Maintenence Programs',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a5.png',
+  },
+  {
+    id: 6,
+    title: 'Plumbing Services',
+    text: `Body text for whatever you'd like to say. Add main takeaway points, quotes, anecdotes, or even a very very short story.`,
+    image: './images/services-a6.png',
+  },
+];
