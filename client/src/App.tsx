f4725fe475540753d9abdcfe43bf1bfb8c7bde29
Devan Rivera <devanrivera98@gmail.com>
2024-06-20 20:54:04 +0000

    updating the form page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
-import CustomerExperience from './components/CustomerExperience/CustomerExperience';
-import Services from './components/ServicesSection/Services';
+// import CustomerExperience from './components/CustomerExperience/CustomerExperience';
+// import Services from './components/ServicesSection/Services';
+import ContactPage from './components/Contact/ContactPage';
 
 export default function App() {
   return (
     <>
-      <CustomerExperience />
-      <Services />
+      <ContactPage />
+      {/* <CustomerExperience />
+      <Services /> */}
     </>
   );
 }
--- client/src/components/Contact/ContactPage.tsx
+++ client/src/components/Contact/ContactPage.tsx
@@ -1,3 +1,40 @@
+import './contactPage.css';
+import ContactForm from './ContactForm';
+
 export default function ContactPage() {
-  return <></>;
+  return (
+    <>
+      <div className="contact-section">
+        <div className="flex ">
+          <div className="w-7/12">
+            <h1>How can we help?</h1>
+            <span>
+              Once we receive the details of your project, a representative will
+              reach out to schedule a meeting. We look forward to partnering
+              with you.
+            </span>
+            <ContactForm />
+          </div>
+          <div className="w-6/12 flex flex-wrap justify-center">
+            <div className="w-11/12">
+              <img src="./images/form-image.png" className="w-full" />
+            </div>
+            <div>
+              <h1>Proud to be employee-owned.</h1>
+              <span>
+                Serving the Washington DC, Maryland, and Virginia area since
+                1997
+              </span>
+              <div>
+                <h1>Advanced Building Services, Inc.</h1>
+                <span>23475 Rock Haven Way #135</span>
+                <span>Sterling, VA 20166</span>
+                <span>(703)661-4280</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
